31dd5ada16c32eafac9a725c940d7d321343d197
tomvdt <tomtommy135@gmail.com>
2024-03-27 15:35:00 +0100

    template ready

---
+++ exercice3/analysis/utils.py
@@ -0,0 +1,58 @@
+import subprocess
+
+# Matplotlib stuff
+rcParams = {
+    "text.usetex": True,
+    "font.family": "serif",
+    "axes.labelsize": 12,
+    "font.size": 12,
+    "legend.fontsize": 10,
+    "xtick.labelsize": 12,
+    "ytick.labelsize": 12,
+    "figure.figsize": (6, 5),
+    "text.latex.preamble": "\n".join([
+        r"\usepackage[utf8]{inputenc}",
+        r"\usepackage[T1]{fontenc}",
+        r"\usepackage[detect-all,locale=FR]{siunitx}",
+    ]),
+    'lines.markersize': 10,
+    'lines.color': 'grey',
+    'scatter.marker': '+',
+    'errorbar.capsize': 3,
+    'savefig.bbox': 'tight',
+}
+
+# Simulation and paths stuff
+path = '../'
+executable = 'bin/ex3'
+export_path = path + 'rapport/figures/'
+data_path = 'data/'
+config_path = 'bin/'
+config_ext = '.conf'
+
+def stringify_dict(d: dict, sep=',') -> str:
+    return sep.join(map(lambda a: str(a[0]) + "=" + str(a[1]), tuple(d.items())))
+
+def run(config_file: str, output_file: str, params: dict = {}) -> None:
+    options = stringify_dict(params, sep=' ')
+    cmd = f"{path}{executable} {path}{config_file} output='{path}{output_file}' {options}"
+    # print(f"Running command `{cmd}`\n", end='')
+    subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL)
+
+def load_conf(config_name: str) -> dict[str, float | str]:
+    conf = {}
+    with open(path + config_path + config_name + config_ext, 'r') as f:
+        lines = f.read().split('\n')
+
+    for line in lines:
+        if not line:
+            continue
+        name, _, val, *_ = line.split(' ')
+        name = name.strip()
+        val = val.strip()
+        try:
+            conf[name] = float(val)
+        except ValueError:
+            conf[name] = val
+    
+    return conf
